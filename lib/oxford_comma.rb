9a4f683e5b805730e2ac3d0e3fc09b7c9a202e87
YanChan  Lin <yonalin1@gmail.com>
2017-10-19 19:43:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  if array.size == 1
+   return array.first
+ elsif array.size == 2
+     return array.join(" and ")
+ else
+   # if there are 3 elements in an array, the last index is 2
+   index = array.size - 1
+  array[index] = "and #{array.last}"
+       return array.join(", ")
 
-end
+   end
+  end
+#
+# result = array.join
+# puts result
+# final_result =result.split(",")
+# final_result<< ("and")
+#
+
+#
+# # << ("shovel") method
