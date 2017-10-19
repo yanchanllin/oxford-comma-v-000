15c135aef132fb00b1ceaea0f6bf3ab9be033382
YanChan  Lin <yonalin1@gmail.com>
2017-10-19 19:41:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,11 @@ def oxford_comma(array)
    return array.first
  elsif array.size == 2
      return array.join(" and ")
- elsif array.size == 3
-       return array.insert(-2, "and").join(", ")
+ else 
+   # if there are 3 elements in an array, the last index is 2
+   index = array.size - 1
+  array[index] = "and #{array.last}"
+       return array.join(", ")
 
    end
   end
