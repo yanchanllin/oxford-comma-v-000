977c2cbefb8ff30e0e25f17190cd1c27e091693e
YanChan  Lin <yonalin1@gmail.com>
2017-10-19 17:02:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,14 @@
 def oxford_comma(array)
-  if array.size == 1 
-   return true 
-  end   
-end   
-#     
+  if array.size == 1
+   return true
+  end
+end
+#
 # result = array.join
 # puts result
 # final_result =result.split(",")
 # final_result<< ("and")
-# 
+#
 
-# 
+#
 # # << ("shovel") method
